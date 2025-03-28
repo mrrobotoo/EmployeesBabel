f3611f7af439759d59b51a0095e5bfe09c057944
Carlos Ruhal Sebastian Ruiz Perez <sebastianperezruiz6@gmail.com>
2025-03-27 19:45:05 -0600

    swagger is added

--- src/main/java/com/babelgroup/employees/controller/EmployeesController.java
+++ src/main/java/com/babelgroup/employees/controller/EmployeesController.java
@@ -18,6 +18,9 @@ import com.babelgroup.employees.dto.ResponseDto;
 import com.babelgroup.employees.entities.EmployeeEntity;
 import com.babelgroup.employees.service.EmployeeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 
@@ -29,23 +32,43 @@ public class EmployeesController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	
+	@Operation(summary = "Get all employees")
 	@GetMapping
 	public ResponseDto<List<EmployeeEntity>> getEmployees() {
 		return employeeService.getEmployees();
 	}
 	
+	@Operation(summary = "Insert multiple employees")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Employees inserted successfully"),
+        @ApiResponse(responseCode = "403", description = "Invalid input data"),
+        @ApiResponse(responseCode = "500", description = "General error")
+
+    })
 	@PostMapping
     public ResponseDto<?> insertEmployees(@RequestBody @Valid List<EmployeeDto> employees) {
         return employeeService.insertEmployees(employees);
     }
 	
+	@Operation(summary = "Delete an employee by ID")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "Employee delete successfully"),
+	        @ApiResponse(responseCode = "500", description = "General error")
+
+	    })
 	@DeleteMapping
 	public ResponseDto<?> deleteEmployees(@RequestParam @Min(1)
 			Long idEmployee) {
 		return employeeService.deleteEmployees(idEmployee);	
 	}
 	
+	@Operation(summary = "Update employee details")
+	@ApiResponses(value = {
+	        @ApiResponse(responseCode = "200", description = "Employee update successfully"),
+	        @ApiResponse(responseCode = "404", description = "Employee not exist"),
+	        @ApiResponse(responseCode = "500", description = "General error")
+
+	    })
 	@PutMapping
 	public ResponseDto<?> updateEmployee(@RequestBody @Valid EmployeeDto employee) {
 		return employeeService.updateEmployee(employee);
